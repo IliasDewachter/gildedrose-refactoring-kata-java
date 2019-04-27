0e1192cfb0e5c6017f94508b083cee2f64caeb5a
Ilias Dewachter <ilias.dewachter@student.kdg.be>
2019-04-27 14:48:21 +0200

    Extracted Sulfuras logic && reverted quality checks

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,46 +9,25 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            if (item.name.equals(ItemNames.SULFURAS)) continue;
 
-            if (!item.name.equals(ItemNames.SULFURAS)) {
-                item.sellIn--;
-            }
+            item.sellIn--;
 
             if (item.name.equals(ItemNames.AGED_BRIE)) {
-                this.increaseQuality(item);
-                if (item.sellIn < 0) {
-                    this.increaseQuality(item);
-                }
+                if (item.sellIn < 0) item.quality += 2;
+                else item.quality++;
             } else if (item.name.equals(ItemNames.BACKSTAGE_PASSES)) {
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                } else {
-                    this.increaseQuality(item);
-                    if (item.sellIn < 10) {
-                        this.increaseQuality(item);
-                        if (item.sellIn < 5) {
-                            this.increaseQuality(item);
-                        }
-                    }
-                }
-            } else if (!item.name.equals(ItemNames.SULFURAS)) {
-                this.decreaseQuality(item);
-                if (item.sellIn < 0) {
-                    this.decreaseQuality(item);
-                }
+                if (item.sellIn < 0) item.quality = 0;
+                else if (item.sellIn < 5) item.quality += 3;
+                else if (item.sellIn < 10) item.quality += 2;
+                else item.quality++;
+            } else {
+                if (item.sellIn < 0) item.quality -= 2;
+                else item.quality--;
             }
+
+            if (item.quality > 50) item.quality = 50;
+            if (item.quality < 0) item.quality = 0;
         }
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.name.equals(ItemNames.SULFURAS)) return;
-        if (item.quality >= 50) return;
-        item.quality++;
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.name.equals(ItemNames.SULFURAS)) return;
-        if (item.quality <= 0) return;
-        item.quality--;
     }
 }
