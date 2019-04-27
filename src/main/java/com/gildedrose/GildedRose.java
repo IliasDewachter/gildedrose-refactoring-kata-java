08d69335d3f8b97642a2deabfdee709184302a47
Ilias Dewachter <ilias.dewachter@student.kdg.be>
2019-04-27 15:04:24 +0200

    Added conjured support

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,6 +21,9 @@ class GildedRose {
                 else if (item.sellIn < 5) item.quality += 3;
                 else if (item.sellIn < 10) item.quality += 2;
                 else item.quality++;
+            } else if (item.name.startsWith("Conjured")) {
+                if (item.sellIn < 0) item.quality -= 4;
+                else item.quality -= 2;
             } else {
                 if (item.sellIn < 0) item.quality -= 2;
                 else item.quality--;
